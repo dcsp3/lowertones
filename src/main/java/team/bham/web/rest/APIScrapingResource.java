15b2a112e169bebc58669463bb855ff10a78caf5
dxp232 <dxp232@student.bham.ac.uk>
2024-04-01 21:11:40 +0100

    adjust network api calls for new types, add network stats, restructure response format

--- src/main/java/team/bham/web/rest/APIScrapingResource.java
+++ src/main/java/team/bham/web/rest/APIScrapingResource.java
@@ -4,9 +4,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.swing.text.StyledEditorKit.BoldAction;
 import org.json.JSONArray;
@@ -83,41 +87,299 @@ public class APIScrapingResource {
     }
 
     @GetMapping("/top-artists-short-term")
-    public ResponseEntity<List<Object>> getShortTermTopArtists(Authentication authentication) {
+    public ResponseEntity<String> getShortTermTopArtists(Authentication authentication) {
         AppUser appUser = resolveAppUser(authentication.getName());
-        JSONObject topArtists = apiWrapper.getCurrentUserTopArtists(appUser, SpotifyTimeRange.SHORT_TERM).getData();
-        JSONArray artists = topArtists.getJSONArray("items");
+        if (appUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
 
-        List<Object> result = new ArrayList<>();
+        SpotifyTimeRange short_term = SpotifyTimeRange.SHORT_TERM;
+
+        SpotifyAPIResponse shortTermArtists = apiWrapper.getCurrentUserTopArtists(appUser, short_term);
+        JSONArray artists = shortTermArtists.getData().getJSONArray("items");
+
+        JSONObject result = new JSONObject();
+        JSONArray graphDataArray = new JSONArray(); // Create a JSONArray to hold all artist info
+
         int min_distance = 100;
         int max_distance = 600;
 
+        int totalPopularity = 0;
+
         for (int count = 0; count < artists.length(); count++) {
             JSONObject item = artists.getJSONObject(count);
+            totalPopularity += item.getInt("popularity"); // Assuming "popularity" is the key for artist popularity
             // Calculate the distance based on rank
             double distance = min_distance + ((double) (max_distance - min_distance) / artists.length()) * (count + 1);
 
-            List<Object> artistInfo = new ArrayList<>();
-            artistInfo.add(distance);
-            artistInfo.add(item.getString("name"));
-            artistInfo.add(item.getString("id"));
+            JSONObject artistInfo = new JSONObject(); // Use JSONObject to hold each artist's info
+            artistInfo.put("distance", distance);
+            artistInfo.put("name", item.getString("name"));
+            artistInfo.put("id", item.getString("id"));
 
             JSONArray genresArray = item.getJSONArray("genres");
             List<String> genresList = genresArray.toList().stream().map(Object::toString).collect(Collectors.toList());
-            artistInfo.add(genresList);
+            artistInfo.put("genres", genresList);
 
-            JSONArray images = item.getJSONArray("images");
-            artistInfo.add(images.getJSONObject(0).getString("url"));
+            if (item.getJSONArray("images").length() > 0) { // Ensure there's at least one image
+                artistInfo.put("imageUrl", item.getJSONArray("images").getJSONObject(0).getString("url"));
+            } else {
+                artistInfo.put("imageUrl", JSONObject.NULL); // Handle case where there's no image
+            }
 
-            result.add(artistInfo);
+            graphDataArray.put(artistInfo); // Add this artist's info to the array
         }
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        double averagePopularity = artists.length() > 0 ? (double) totalPopularity / artists.length() : 0;
+
+        String tasteCategory;
+
+        if (averagePopularity < 20) {
+            tasteCategory = "Underground";
+        } else if (averagePopularity < 40) {
+            tasteCategory = "Rising";
+        } else if (averagePopularity < 60) {
+            tasteCategory = "Cool";
+        } else if (averagePopularity < 80) {
+            tasteCategory = "Trending";
+        } else if (averagePopularity < 90) {
+            tasteCategory = "Mainstream";
+        } else {
+            tasteCategory = "Superstar";
+        }
+
+        // Prepare stats object
+        JSONObject stats = new JSONObject();
+
+        if (artists.length() > 0) {
+            // Assuming the first artist is the top artist
+            JSONObject topArtist = artists.getJSONObject(0);
+            stats.put("topArtistName", topArtist.getString("name"));
+            stats.put("topArtistImage", topArtist.getJSONArray("images").getJSONObject(0).getString("url"));
+
+            // Calculate the top genre
+            Map<String, Integer> genreCounts = new HashMap<>();
+            for (int i = 0; i < artists.length(); i++) {
+                JSONArray genres = artists.getJSONObject(i).getJSONArray("genres");
+                for (int j = 0; j < genres.length(); j++) {
+                    String genre = genres.getString(j);
+                    genreCounts.put(genre, genreCounts.getOrDefault(genre, 0) + 1);
+                }
+            }
+            String topGenre = Collections.max(genreCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
+            stats.put("topGenre", topGenre);
+
+            // Update stats object
+            stats.put("averagePopularity", String.format("%.2f%%", averagePopularity));
+            stats.put("tasteCategory", tasteCategory);
+        }
+
+        result.put("graphData", graphDataArray);
+        result.put("stats", stats);
+
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
     }
 
     @GetMapping("/top-artists-medium-term")
-    public ResponseEntity<List<Object>> getMediumTermTopArtists(Authentication authentication) {
+    public ResponseEntity<String> getMediumtTermTopArtists(Authentication authentication) {
         AppUser appUser = resolveAppUser(authentication.getName());
+        if (appUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        SpotifyTimeRange medium_term = SpotifyTimeRange.MEDIUM_TERM;
+
+        SpotifyAPIResponse mediumTermArtists = apiWrapper.getCurrentUserTopArtists(appUser, medium_term);
+        JSONArray artists = mediumTermArtists.getData().getJSONArray("items");
+
+        JSONObject result = new JSONObject();
+        JSONArray graphDataArray = new JSONArray(); // Create a JSONArray to hold all artist info
+
+        int min_distance = 100;
+        int max_distance = 600;
+
+        int totalPopularity = 0;
+
+        for (int count = 0; count < artists.length(); count++) {
+            JSONObject item = artists.getJSONObject(count);
+            totalPopularity += item.getInt("popularity"); // Assuming "popularity" is the key for artist popularity
+            // Calculate the distance based on rank
+            double distance = min_distance + ((double) (max_distance - min_distance) / artists.length()) * (count + 1);
+
+            JSONObject artistInfo = new JSONObject(); // Use JSONObject to hold each artist's info
+            artistInfo.put("distance", distance);
+            artistInfo.put("name", item.getString("name"));
+            artistInfo.put("id", item.getString("id"));
+
+            JSONArray genresArray = item.getJSONArray("genres");
+            List<String> genresList = genresArray.toList().stream().map(Object::toString).collect(Collectors.toList());
+            artistInfo.put("genres", genresList);
+
+            if (item.getJSONArray("images").length() > 0) { // Ensure there's at least one image
+                artistInfo.put("imageUrl", item.getJSONArray("images").getJSONObject(0).getString("url"));
+            } else {
+                artistInfo.put("imageUrl", JSONObject.NULL); // Handle case where there's no image
+            }
+
+            graphDataArray.put(artistInfo); // Add this artist's info to the array
+        }
+
+        double averagePopularity = artists.length() > 0 ? (double) totalPopularity / artists.length() : 0;
+
+        String tasteCategory;
+
+        if (averagePopularity < 20) {
+            tasteCategory = "Underground";
+        } else if (averagePopularity < 40) {
+            tasteCategory = "Rising";
+        } else if (averagePopularity < 60) {
+            tasteCategory = "Cool";
+        } else if (averagePopularity < 80) {
+            tasteCategory = "Trending";
+        } else if (averagePopularity < 90) {
+            tasteCategory = "Mainstream";
+        } else {
+            tasteCategory = "Superstar";
+        }
+
+        // Prepare stats object
+        JSONObject stats = new JSONObject();
+
+        if (artists.length() > 0) {
+            // Assuming the first artist is the top artist
+            JSONObject topArtist = artists.getJSONObject(0);
+            stats.put("topArtistName", topArtist.getString("name"));
+            stats.put("topArtistImage", topArtist.getJSONArray("images").getJSONObject(0).getString("url"));
+
+            // Calculate the top genre
+            Map<String, Integer> genreCounts = new HashMap<>();
+            for (int i = 0; i < artists.length(); i++) {
+                JSONArray genres = artists.getJSONObject(i).getJSONArray("genres");
+                for (int j = 0; j < genres.length(); j++) {
+                    String genre = genres.getString(j);
+                    genreCounts.put(genre, genreCounts.getOrDefault(genre, 0) + 1);
+                }
+            }
+            String topGenre = Collections.max(genreCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
+            stats.put("topGenre", topGenre);
+
+            // Update stats object
+            stats.put("averagePopularity", String.format("%.2f%%", averagePopularity));
+            stats.put("tasteCategory", tasteCategory);
+        }
+
+        result.put("graphData", graphDataArray);
+        result.put("stats", stats);
+
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    }
+
+    @GetMapping("/top-artists-long-term")
+    public ResponseEntity<String> getLongTermTopArtists(Authentication authentication) {
+        AppUser appUser = resolveAppUser(authentication.getName());
+        if (appUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        SpotifyTimeRange long_term = SpotifyTimeRange.LONG_TERM;
+
+        SpotifyAPIResponse longTermArtists = apiWrapper.getCurrentUserTopArtists(appUser, long_term);
+        JSONArray artists = longTermArtists.getData().getJSONArray("items");
+
+        JSONObject result = new JSONObject();
+        JSONArray graphDataArray = new JSONArray(); // Create a JSONArray to hold all artist info
+
+        int min_distance = 100;
+        int max_distance = 600;
+
+        int totalPopularity = 0;
+
+        for (int count = 0; count < artists.length(); count++) {
+            JSONObject item = artists.getJSONObject(count);
+            totalPopularity += item.getInt("popularity"); // Assuming "popularity" is the key for artist popularity
+            // Calculate the distance based on rank
+            double distance = min_distance + ((double) (max_distance - min_distance) / artists.length()) * (count + 1);
+
+            JSONObject artistInfo = new JSONObject(); // Use JSONObject to hold each artist's info
+            artistInfo.put("distance", distance);
+            artistInfo.put("name", item.getString("name"));
+            artistInfo.put("id", item.getString("id"));
+
+            JSONArray genresArray = item.getJSONArray("genres");
+            List<String> genresList = genresArray.toList().stream().map(Object::toString).collect(Collectors.toList());
+            artistInfo.put("genres", genresList);
+
+            if (item.getJSONArray("images").length() > 0) { // Ensure there's at least one image
+                artistInfo.put("imageUrl", item.getJSONArray("images").getJSONObject(0).getString("url"));
+            } else {
+                artistInfo.put("imageUrl", JSONObject.NULL); // Handle case where there's no image
+            }
+
+            graphDataArray.put(artistInfo); // Add this artist's info to the array
+        }
+
+        double averagePopularity = artists.length() > 0 ? (double) totalPopularity / artists.length() : 0;
+
+        String tasteCategory;
+
+        if (averagePopularity < 20) {
+            tasteCategory = "Underground🤘";
+        } else if (averagePopularity < 40) {
+            tasteCategory = "Rising📈";
+        } else if (averagePopularity < 60) {
+            tasteCategory = "Cool😎";
+        } else if (averagePopularity < 80) {
+            tasteCategory = "Trending🔥";
+        } else if (averagePopularity < 90) {
+            tasteCategory = "Mainstream🌍";
+        } else {
+            tasteCategory = "Superstar⭐";
+        }
+
+        // Prepare stats object
+        JSONObject stats = new JSONObject();
+
+        if (artists.length() > 0) {
+            // Assuming the first artist is the top artist
+            JSONObject topArtist = artists.getJSONObject(0);
+            stats.put("topArtistName", topArtist.getString("name"));
+            stats.put("topArtistImage", topArtist.getJSONArray("images").getJSONObject(0).getString("url"));
+
+            // Calculate the top genre
+            Map<String, Integer> genreCounts = new HashMap<>();
+            for (int i = 0; i < artists.length(); i++) {
+                JSONArray genres = artists.getJSONObject(i).getJSONArray("genres");
+                for (int j = 0; j < genres.length(); j++) {
+                    String genre = genres.getString(j);
+                    genreCounts.put(genre, genreCounts.getOrDefault(genre, 0) + 1);
+                }
+            }
+            String topGenre = Collections.max(genreCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
+            stats.put("topGenre", topGenre);
+
+            // Update stats object
+            stats.put("averagePopularity", String.format("%.2f%%", averagePopularity));
+            stats.put("tasteCategory", tasteCategory);
+        }
+
+        result.put("graphData", graphDataArray);
+        result.put("stats", stats);
+
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    }
+
+    /*
+
+    @GetMapping("/top-artists-medium-term")
+    public ResponseEntity<String> getMediumTermTopArtists(Authentication authentication) {
+        AppUser appUser = resolveAppUser(authentication.getName());
+        if (appUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        
+        SpotifyTimeRange medium_term = SpotifyTimeRange.MEDIUM_TERM;
+
+
         JSONObject topArtists = apiWrapper.getCurrentUserTopArtists(appUser, SpotifyTimeRange.MEDIUM_TERM).getData();
         JSONArray artists = topArtists.getJSONArray("items");
 
@@ -149,8 +411,15 @@ public class APIScrapingResource {
     }
 
     @GetMapping("/top-artists-long-term")
-    public ResponseEntity<List<Object>> getLongTermTopArtists(Authentication authentication) {
+    public ResponseEntity<String> getLongTermTopArtists(Authentication authentication) {
         AppUser appUser = resolveAppUser(authentication.getName());
+        if (appUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        
+        SpotifyTimeRange long_term = SpotifyTimeRange.LONG_TERM;
+
+
         JSONObject topArtists = apiWrapper.getCurrentUserTopArtists(appUser, SpotifyTimeRange.LONG_TERM).getData();
         JSONArray artists = topArtists.getJSONArray("items");
 
@@ -180,8 +449,8 @@ public class APIScrapingResource {
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
+  */
 
-    /* 
     @GetMapping("/top-artists")
     public ResponseEntity<String> getCurrentUserTopArtists(Authentication authentication) {
         AppUser appUser = resolveAppUser(authentication.getName());
@@ -189,22 +458,25 @@ public class APIScrapingResource {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
 
+        SpotifyTimeRange short_term = SpotifyTimeRange.SHORT_TERM;
+        SpotifyTimeRange medium_term = SpotifyTimeRange.MEDIUM_TERM;
+        SpotifyTimeRange long_term = SpotifyTimeRange.LONG_TERM;
+
         try {
-            JSONObject shortTermArtists = apiWrapper.getCurrentUserShortTermTopArtists(appUser);
-            JSONObject mediumTermArtists = apiWrapper.getCurrentUserMediumTermTopArtists(appUser);
-            JSONObject longTermArtists = apiWrapper.getCurrentUserLongTermTopArtists(appUser);
+            SpotifyAPIResponse shortTermArtists = apiWrapper.getCurrentUserTopArtists(appUser, short_term);
+            SpotifyAPIResponse mediumTermArtists = apiWrapper.getCurrentUserTopArtists(appUser, medium_term);
+            SpotifyAPIResponse longTermArtists = apiWrapper.getCurrentUserTopArtists(appUser, long_term);
 
             JSONObject result = new JSONObject();
-            result.put("shortTerm", shortTermArtists);
-            result.put("mediumTerm", mediumTermArtists);
-            result.put("longTerm", longTermArtists);
+            result.put("shortTerm", shortTermArtists.getData());
+            result.put("mediumTerm", mediumTermArtists.getData());
+            result.put("longTerm", longTermArtists.getData());
 
             return new ResponseEntity<>(result.toString(), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to retrieve artist data");
         }
     }
-    */
 
     private AppUser resolveAppUser(String name) {
         User user = userRepository.findOneByLogin(name).get();
