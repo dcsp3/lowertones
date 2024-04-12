66ca62b159c5d36be7ba1aa9f6fb9a4d4a6f87b4
Henry Southall <hxs362@student.bham.ac.uk>
2024-04-12 18:40:44 +0100

    add misc attribs to SpotifyArtist

--- src/main/java/team/bham/service/APIWrapper/SpotifyArtist.java
+++ src/main/java/team/bham/service/APIWrapper/SpotifyArtist.java
@@ -3,14 +3,32 @@ package team.bham.service.APIWrapper;
 public class SpotifyArtist {
 
     private String name;
+    private String spotifyId;
+    private int popularity;
 
-    //todo: more shit for this
+    //todo: more shit for this (img, etc.)
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setSpotifyId(String spotifyId) {
+        this.spotifyId = spotifyId;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public String getSpotifyId() {
+        return spotifyId;
+    }
+
+    public int getPopularity() {
+        return popularity;
     }
 }
