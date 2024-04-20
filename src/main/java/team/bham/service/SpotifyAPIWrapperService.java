15873ab43c4b9ad8648b27af726124bb4dd27740
Henry Southall <hxs362@student.bham.ac.uk>
2024-04-20 21:08:05 +0100

    fdjkdskjdfskfskjls

--- src/main/java/team/bham/service/SpotifyAPIWrapperService.java
+++ src/main/java/team/bham/service/SpotifyAPIWrapperService.java
@@ -199,7 +199,7 @@ public class SpotifyAPIWrapperService {
             try {
                 audioFeaturesListJSON = response.getJSONArray("audio_features");
             } catch (Exception e) {
-                throw new RuntimeException("what the fuck?" + test.getSuccess() + " " + test.getStatus());
+                throw new RuntimeException("what the fuck?" + test.getSuccess() + " " + test.getStatus() + curEndpoint);
             }
 
             for (int j = 0; j < audioFeaturesListJSON.length(); j++) {
@@ -255,6 +255,7 @@ public class SpotifyAPIWrapperService {
 
             if (status != HttpStatus.UNAUTHORIZED) {
                 apiResponse.setSuccess(false);
+                apiResponse.setStatus(status);
                 return apiResponse;
             }
 
